7cc930c5d480a6a307fa18d712fb4511276004d2
Martin Vilcans <martin@librador.com>
2015-07-26 16:38:43 +0200

    Lock/unlock

--- busslarm.ino
+++ busslarm.ino
@@ -19,7 +19,7 @@ The signal pin is typically yellow, orange or white and should be connected to a
 Servo steeringServo;
 
 const int led = 13;
-//const int buzzerPin = 11;  // PWM
+const int buzzerPin = 2;  // PWM
 const int motorPin = 9;  // PWM
 const int motorPin2 = 10;  // PWM
 const int buttonPin = 12;
@@ -29,6 +29,7 @@ const int joyRightPin = A1;
 const int joyLeftPin = A2;
 const int joyDownPin = A3;
 const int joyUpPin = A4;
+const int joyButtonPin = A5;
 
 const int MID_ANGLE = 67;
 const int MAX_ANGLE = 86;
@@ -40,7 +41,7 @@ int speed = 0;
 void setup() {                
   pinMode(led, OUTPUT);     
   pinMode(buttonPin, INPUT_PULLUP);
-  //pinMode(buzzerPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
   pinMode(motorPin, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(servoPin, OUTPUT);
@@ -50,42 +51,12 @@ void setup() {
   pinMode(joyLeftPin, INPUT_PULLUP);
   pinMode(joyDownPin, INPUT_PULLUP);
   pinMode(joyUpPin, INPUT_PULLUP);
+  pinMode(joyButtonPin, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
-void loop() {
-
-  while(true) {
-    const int delayTime = 3000;
-    const int middleAngle = 90 + 25;
-
-#if 0
-    Serial.println("1");
-    steeringServo.write(middleAngle - 25);
-    analogWrite(motorPin, 0);
-    analogWrite(motorPin2, 255);
-    delay(delayTime);
-
-    Serial.println("2");
-    steeringServo.write(middleAngle);
-    analogWrite(motorPin, 0);
-    analogWrite(motorPin2, 0);
-    delay(delayTime);
-
-    Serial.println("3");
-    steeringServo.write(middleAngle + 25);
-    steeringServo.write(90);
-    analogWrite(motorPin, 255);
-    analogWrite(motorPin2, 0);
-    delay(delayTime);
-
-    Serial.println("4");
-    steeringServo.write(middleAngle);
-    steeringServo.write(45);
-    analogWrite(motorPin, 0);
-    analogWrite(motorPin2, 0);
-    delay(delayTime * 2);
-#else
+void activeState() {
+  while(digitalRead(joyButtonPin) == HIGH) {
     int newAngle = steeringAngle;
     if(digitalRead(joyRightPin) == LOW && newAngle < MAX_ANGLE) {
       newAngle += 1;
@@ -123,15 +94,18 @@ void loop() {
       Serial.println(speed);
     }
     delay(20);
-#endif
   }
+}
 
+void lockedState() {
+  tone(buzzerPin, 440);
+  delay(200);
+  tone(buzzerPin, 220);
+  delay(200);
+  noTone(buzzerPin);
 
-#if 0
-  if(true) {
+  while(digitalRead(joyButtonPin) == HIGH) {
     int buttonState = digitalRead(buttonPin);
-    Serial.print("state ");
-    Serial.print(buttonState);
     if(buttonState == LOW) {
       digitalWrite(led, HIGH);
     }
@@ -147,7 +121,18 @@ void loop() {
       noTone(buzzerPin);
     }
   }
-#endif
+  tone(buzzerPin, 220);
+  delay(200);
+  tone(buzzerPin, 440);
+  delay(200);
+  noTone(buzzerPin);
+}
+
+void loop() {
+  Serial.println("Låser upp...");
+  activeState();
+  Serial.println("Låser...");
+  lockedState();
 }
 
 /* vim: set ts=2 sw=2 tw=0 syntax=cpp et :*/
